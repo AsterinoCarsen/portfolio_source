a6551f4d9d30e9af3f9dba2c775dcc7658f536c1
Carsen Asterino <carsenasterino@gmail.com>
2022-05-23 23:04:56 -0700

    Published version 1.0

--- src/App.js
+++ src/App.js
@@ -22,10 +22,12 @@ function App() {
     <div className="App">
       <NavBar />
       <Hero />
+      <AboutMe />
       <MyTools />
-      <Testimonials />
       <Education />
+      <Testimonials />
       <Projects />
+      <Contact />
     </div>
   );
 }
@@ -102,17 +104,18 @@ class Testimonials extends React.Component {
 
 function NavBar() {
   return (
-    <div className='flex w-full h-20 bg-eerieBlack justify-center items-center'>
+    <div className='flex fixed w-full h-20 bg-eerieBlack justify-center items-center z-50'>
       <div className='flex flex-row w-3/4 h-full text-antiqueWhite'>
         <div className='flex w-1/2 h-full justify-start items-center'>
-          <a href='https://github.com/AsterinoCarsen/portfolio_source' target="_blank">Made with <span className='text-red'>❤</span>, by Carsen Asterino, on GitHub</a>
+          <a href='https://github.com/AsterinoCarsen/portfolio_source' target="_blank" className='hover:-translate-y-1 hover:scale-105 duration-200'>Made with <span className='text-red'>❤</span>, by Carsen Asterino, on GitHub</a>
         </div>
         
         <div className='flex w-1/2 h-full justify-end items-center'>
-          <p className='pl-5 pr-5 cursor-pointer hover:-translate-y-1 hover:scale-105 duration-200'>Home</p>
-          <p className='pl-5 pr-5 cursor-pointer hover:-translate-y-1 hover:scale-105 duration-200'>About</p>
-          <p className='pl-5 pr-5 cursor-pointer hover:-translate-y-1 hover:scale-105 duration-200'>Education</p>
-          <p className='pl-5 pr-5 cursor-pointer hover:-translate-y-1 hover:scale-105 duration-200'>Portfolio</p>
+          <a href='#home'><p className='pl-5 pr-5 cursor-pointer hover:-translate-y-1 hover:scale-105 duration-200'>Home</p></a>
+          <a href='#aboutme'><p className='pl-5 pr-5 cursor-pointer hover:-translate-y-1 hover:scale-105 duration-200'>About</p></a>
+          <a href='#education'><p className='pl-5 pr-5 cursor-pointer hover:-translate-y-1 hover:scale-105 duration-200'>Education</p></a>
+          <a href='#projects'><p className='pl-5 pr-5 cursor-pointer hover:-translate-y-1 hover:scale-105 duration-200'>Projects</p></a>
+          <a href='#contact'><p className='pl-5 pr-5 cursor-pointer hover:-translate-y-1 hover:scale-105 duration-200'>Contact</p></a>
         </div>
       </div>
     </div>
@@ -121,12 +124,12 @@ function NavBar() {
 
 function Hero() {
   return (
-    <div className='flex w-full bg-eerieBlack justify-center items-center overflow-y-clip' style={ { height: "55vh"} }>
+    <div className='flex w-full bg-eerieBlack justify-center items-center overflow-y-clip' style={ { height: "55vh"} } id='home'>
       <div className='flex w-3/4 h-full justify-center items-center'>
         <div className='flex-row w-1/2 justify-start font-ver text-antiqueWhite'>
           <p className='font-thin pb-2'>WELCOME TO MY WORLD</p>
           <h1 className='text-5xl font-bold'>Hello, I'm <span className='text-oldRose'>Carsen.</span></h1>
-          <p className='pt-6 text-base'>Quickly design and customize responsive mobile-first sites with Tailwind, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+          <p className='pt-6 text-base'>I'm a passionate, self-taught developer, who works in several different disciplines, motivated by my curiosity. My areas of expertise are in Front-End development using React and TailwindCSS. Additionally, I'm highly proficient with Unity 2D game development in C#.</p>
         </div>
 
         <div className='flex w-1/2 justify-end items-center'>
@@ -139,7 +142,7 @@ function Hero() {
 
 function MyTools() {
   return (
-    <div className='flex flex-col w-full bg-blackCoffee justify-center items-center overflow-y-clip' style={ { height: "65vh" }}>
+    <div className='flex flex-col w-full bg-eerieBlack justify-center items-center overflow-y-clip' style={ { height: "65vh" }}>
       <h1 className='pb-20 text-antiqueWhite text-5xl font-ver font-bold'>Tools I Use</h1>
       <div className='flex flex-col'>
         <div className='flex gap-48 w-full h-1/2 pb-32'>
@@ -160,10 +163,10 @@ function MyTools() {
 
 function Education() {
   return (
-    <div className='flex w-full items-center justify-center bg-blackCoffee' style={ { height: "65vh" } }>
+    <div className='flex w-full items-center justify-center bg-blackCoffee' style={ { height: "65vh" } } id='education'>
       <div className='flex flex-col w-3/4 h-3/4 bg-eerieBlack rounded-lg p-12 drop-shadow-2xl'>
-        <h1 className='text-4xl font-bold font-ver text-antiqueWhite border-b-2 border-white'>Education</h1>
-        <div className='flex flex-row h-1/2 w-full border-b-2 border-white'>
+        <h1 className='text-4xl font-bold font-ver text-antiqueWhite pb-2 border-b border-eerieBlackLight'>Education</h1>
+        <div className='flex flex-row h-1/2 w-full border-b border-eerieBlackLight'>
           <div className='flex flex-col h-full w-1/2 justify-center'>
             <h1 className='font-ver text-1xl text-antiqueWhite font-semibold'>Spokane Falls Community College</h1>
             <p className='font-ver text-sm text-gray'>September 2020 - June 2022</p>
@@ -172,7 +175,7 @@ function Education() {
 
           <div className='flex flex-col h-full w-1/2 justify-center items-start'>
             <h1 className='font-ver text-1xl text-antiqueWhite font-bold'>Associate's in Arts AA-DTA</h1>
-            <p className='font-ver text-sm text-gray'>Related Coursework: MATH&147, BUS&101, ENGL&235</p>
+            <p className='font-ver text-sm text-gray'>Related Coursework: MATH&147, BUS&201, ENGL&235</p>
           </div>
         </div>
 
@@ -195,7 +198,7 @@ function Education() {
 
 function Projects() {
   return (
-    <div className='flex flex-col w-full bg-eerieBlack' style={ { height: "100vh" } }>
+    <div className='flex flex-col w-full bg-blackCoffee' style={ { height: "100vh" } } id='projects'>
       <div className='flex w-full h-1/6 items-center justify-center'><h1 className='font-ver text-antiqueWhite text-5xl font-bold'>My Projects</h1></div>
 
       <div className='grid grid-rows-2 grid-cols-2 w-full h-full p-10 pl-64 pr-64 gap-10 '>
@@ -211,7 +214,7 @@ function Projects() {
         <div className='flex w-auto h-auto hover:-translate-y-1 hover:scale-105 duration-200' style={ { backgroundImage: `url(${ib})` } }>
           <a href='https://game.innovatorsbox.com/' target="_blank" className='w-full h-full'>
             <div className='flex w-full h-full backdrop-blur-sm backdrop-brightness-50 justify-center items-center'>
-              <h1 className='pb-4 font-ver text-antiqueWhite font-semibold' style={ { fontSize: "7vh" } }>Innovator's Box</h1>
+              <h1 className='pb-4 font-ver text-antiqueWhite font-semibold text-center overflow-hidden' style={ { fontSize: "7vh" } }>Innovator's Box</h1>
             </div>
           </a>
         </div>
@@ -232,6 +235,65 @@ function Projects() {
           </a>
         </div>
 
+      </div>
+    </div>
+  )
+}
+
+function Contact() {
+  return (
+    <div className='flex flex-col h-48 w-full bg-eerieBlack items-center' id='contact'>
+      <h1 className='font-ver w-3/4 text-2xl text-antiqueWhite text-center font-semibold pt-5 pb-5 border-b border-eerieBlackLight'>Get in touch</h1>
+      <div className='flex flex-row justify-around items-center w-full h-full bg-eerieBlack text-red'>
+        <a href='mailto: carse.astro@gmail.com' target="_blank">carse.astro@gmail.com</a>
+        <p>+1 (509) 993-6711</p>
+      </div>
+    </div>
+  )
+}
+
+function AboutMe() {
+  return (
+    <div className='flex w-full h-auto bg-blackCoffee justify-center pb-16' id='aboutme'>
+      <div className='flex flex-col w-3/4 h-full'>
+        <h1 className='font-ver font-bold text-antiqueWhite text-5xl text-center pt-10'>About Me</h1>
+        <h2 className='text-left font-ver font-normal text-antiqueWhite text-3xl pt-14 pb-2 border-b-2 border-blackCoffeeLight'>Background</h2>
+        <p className='text-left font-ver font-thin text-antiqueWhite text-lg pt-4 pb-2'>Over the last five years, I've honed my development skills via online resources and self-teaching.  Motivated by my curiosity, I've completed the following:</p>
+        
+        <div className='flex w-full h-auto pl-8 pt-2'>
+          <ol class="relative border-l border-red">                  
+            <li class="mb-10 ml-4">
+              <div class="absolute w-3 h-3 bg-red rounded-full mt-1.5 -left-1.5 border border-red"></div>
+              <time class="mb-1 text-sm font-normal leading-none text-gray font-ver">March 2017</time>
+              <h3 class="text-lg font-semibold text-antiqueWhite">Began learning mod-tools for popular video games</h3>
+              <p class="mb-4 text-base font-normal text-gray font-ver">3D level design, scripting in custom languages.</p>
+            </li>
+            <li class="mb-10 ml-4">
+              <div class="absolute w-3 h-3 bg-red rounded-full mt-1.5 -left-1.5 border border-red"></div>
+              <time class="mb-1 text-sm font-normal leading-none text-gray font-ver">March 2018</time>
+              <h3 class="text-lg font-semibold text-antiqueWhite">Learning of Unity 2D and C#</h3>
+              <p class="mb-4 text-base font-normal text-gray font-ver">Using online courses and resources, I began to write my first real code in C# with Unity.</p>
+            </li>
+            <li class="mb-10 ml-4">
+              <div class="absolute w-3 h-3 bg-red rounded-full mt-1.5 -left-1.5 border border-red"></div>
+              <time class="mb-1 text-sm font-normal leading-none text-gray font-ver">January 2020</time>
+              <h3 class="text-lg font-semibold text-antiqueWhite">Freelance development of Unity 2D games</h3>
+              <p class="mb-4 text-base font-normal text-gray font-ver">Harnessing what I've learned the last two years, I started to write my first production code.</p>
+            </li>
+            <li class="mb-10 ml-4">
+              <div class="absolute w-3 h-3 bg-red rounded-full mt-1.5 -left-1.5 border border-red"></div>
+              <time class="mb-1 text-sm font-normal leading-none text-gray font-ver">February 2021</time>
+              <h3 class="text-lg font-semibold text-antiqueWhite">Learning of HTML, JavaScript, and CSS</h3>
+              <p class="mb-4 text-base font-normal text-gray font-ver">In order to expand my skillset, I decided to learn web development, starting with the basics.</p>
+            </li>
+            <li class="ml-4">
+              <div class="absolute w-3 h-3 bg-red rounded-full mt-1.5 -left-1.5 border border-red"></div>
+              <time class="mb-1 text-sm font-normal leading-none text-gray font-ver">March 2022</time>
+              <h3 class="text-lg font-semibold text-antiqueWhite">Learning of React and TailwindCSS</h3>
+              <p class="mb-4 text-base font-normal text-gray font-ver">To take my web development skills to the next level, it was paramount to learn frameworks and libraries.</p>
+            </li>
+          </ol>
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,10 @@ module.exports = {
     extend: {},
     colors: {
       'eerieBlack': '#1c1c1c',
+      'eerieBlackLight': '#474747',
       'antiqueWhite': '#faebd7',
       'blackCoffee': '#362b2b',
+      'blackCoffeeLight': '#665252',
       'oldRose': '#CB8589',
       'mountPink': '#94849B',
       'white': '#FFFFFF',
